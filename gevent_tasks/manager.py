c14ae0776cc3f8c76c7188aeeaecaf5f1783fd93
Blake VandeMerwe <blakev@email.com>
2017-10-18 14:18:46 -0600

    Fix logic and Typing information

--- gevent_tasks/manager.py
+++ gevent_tasks/manager.py
@@ -4,8 +4,8 @@
 #     gevent-tasks, 2017
 # <<
 
-import math
-from logging import getLogger
+from typing import List
+from logging import Logger, getLogger
 from collections import OrderedDict
 
 from gevent.pool import Pool
@@ -14,29 +14,20 @@ from gevent_tasks.tasks import Task, TaskPool
 
 
 class TaskManager(object):
-    def __init__(self, pool=None, logger=None, *tasks):
-        # type: (Pool, *Task) -> self
+    def __init__(self, pool=None, logger=None):
+        # type: (Pool, Logger) -> self
 
         size = None
-
         if isinstance(pool, int):
             size = pool
-
         if pool is None:
-            if tasks:
-                size = math.ceil(len(tasks) * 0.75)
-            else:
-                size = 5
-
+            size = TaskPool.DEFAULT_POOL_SIZE
         if size is not None:
             pool = TaskPool(size=size)
 
         self._pool = pool
         self._tasks = OrderedDict()
         self.logger = logger or getLogger('%s.TaskManager' % __name__)
-
-        for task in tasks:
-            self.add(task)
 
     def __repr__(self):
         return '<TaskManager(tasks=%d,capacity=%d)>' % (
@@ -48,10 +39,12 @@ class TaskManager(object):
 
     @property
     def pool(self):
+        # type: () -> TaskPool
         return self._pool
 
     @property
     def task_names(self):
+        # type: () -> List[str]
         return [t for t in self._tasks.keys()]
 
     def add(self, task, start=False):
@@ -62,6 +55,11 @@ class TaskManager(object):
         self._tasks[task.name] = task
         if start and not task.is_running:
             task.start()
+
+    def add_many(self, *tasks, start=False):
+        # type: (*Task, bool) -> None
+        for task in tasks:
+            self.add(task, start=start)
 
     def start(self, task_name):
         # type: (str) -> None
@@ -86,6 +84,7 @@ class TaskManager(object):
             self.stop(task, force)
 
     def remove_task(self, task, force=False):
+        # type: (Task, bool) -> None
         if hasattr(task, 'name'):
             name = task.name
         else:
