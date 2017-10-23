ff5eccb59efd09cfdeb64150440de35215e1b77d
Blake VandeMerwe <blakev@email.com>
2017-10-23 13:37:12 -0600

    Fix `gen_uuid` logic and documentation

--- gevent_tasks/utils.py
+++ gevent_tasks/utils.py
@@ -11,6 +11,23 @@ ch_choices = string.ascii_letters + string.digits
 
 
 def gen_uuid(length=4):
-    # type: (int) -> str
-    """ Generate a random ID of a given length. """
+    """ Generate a random ID of a given length.
+
+        Args:
+            length (int): length of the returned string.
+
+        Returns:
+            `str` of length ``length``.
+
+        Example::
+
+            >>> gen_uuid()
+            aB6z
+            >>> gen_uuid(10)
+            aAzZ0123mN
+            >>> gen_uuid(None)
+            9
+    """
+    if not length or length < 1:
+        length = 1
     return ''.join(map(lambda c: random.choice(ch_choices), range(length)))
